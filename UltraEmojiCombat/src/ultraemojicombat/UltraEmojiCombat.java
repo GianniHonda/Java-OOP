b98fd5909c4262a12f5d8b7ce67d123df91f4f37
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2024-09-06 11:35:24 +0900

    Aggregation between Objects

--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -14,8 +14,10 @@ public class UltraEmojiCombat {
         l[4] = new Lutador ("UF0Cobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         
         l[5] = new Lutador ("Nerdart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
-        
-        l[3].status();
+       
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[4], l[5]);
+        UEC01.lutar();
     }
     
 }
