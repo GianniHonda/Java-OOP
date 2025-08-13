4eab1a14d10f7f123265a26344c3be2824651747
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2025-08-13 17:32:52 +0900

    Training maths operator’s

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //tipo de dado + identificacao + valor atribuido
+       /*byte idade = 120;
+       short estoque = 32000;
+       int numero = 2147483647;
+       long contaCorrente = 9223372036854775807L; 
+       float real = 10.50f;
+       double dolar =50.864560394857327;
+       String nome = "Gianni Stephanie";
+       char minhaLetra = 'A';
+       boolean botaoAtivado = true;
+       boolean botaoDesativado = false;
+
+       var sobrenome = "Carreira Honda";
+
+       //Classes Wrappers
+       Integer numeros = 2343454;
+       Character meusNumeros = '2';*/
+
+       //Multabilidade e imultabilidade
+       /*Mutaveis 
+       byte idade = 30;
+
+       idade = 50;
+
+       idade = 120;*/
+
+       /*Imutaveis ou constantes 
+       final String NOME = "Carlos";
+       */
+
+       /*Concatenacao */
+       /*String nome;
+       Byte idade;
+       String profissao;
+
+       System.out.println("Qual e o seu nome?"); 
+       Scanner entradaUsuario = new Scanner(System.in);
+       nome = entradaUsuario.nextLine();
+       System.out.println("Qual e a sua profissao?");
+       profissao = entradaUsuario.nextLine();
+       System.out.println("Qual e a sua idade?");
+       idade = entradaUsuario.nextByte();*/
+
+       int numero1 = 1;
+       int numero2 = 7;
+
+       Scanner entradaUsuario = new Scanner(System.in);
+
+       System.out.println(numero1 % numero2);
+    }
+}
